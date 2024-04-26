afa22c98817eba0270dc18dfd305277081092c41
Benjamin-Bergman <git@gdane.net>
2024-04-26 10:59:26 -0400

    Lanterna boilerplate

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,8 +2,24 @@
 
 package com.pluralsight;
 
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.terminal.*;
+
+import java.io.*;
+
 final class Program {
     public static void main(String[] args) {
+        try (var screen = new DefaultTerminalFactory().createScreen()) {
+            screen.startScreen();
 
+            var gui = new MultiWindowTextGUI(screen);
+            var window = new BasicWindow();
+            gui.addWindow(window);
+            window.waitUntilClosed();
+
+            screen.stopScreen();
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
     }
 }
