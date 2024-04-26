2c6f2388a64014612fa6d42baace9369c51bcb85
Benjamin-Bergman <git@gdane.net>
2024-04-26 10:06:54 -0400

    Transaction serialization

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -3,6 +3,8 @@
 package com.pluralsight;
 
 import java.time.*;
+import java.time.format.*;
+import java.util.*;
 
 /**
  * Represents a financial transaction.
@@ -13,6 +15,37 @@ import java.time.*;
  * @param amount      How much money this transaction exchanged.
  */
 public record Transaction(LocalDateTime dateTime, String description, String vendor, Double amount) {
+    /**
+     * Deserializes a transaction from a row of CSV.
+     *
+     * @param csv The CSV row to deserialize.
+     * @return A CSV representation of this transaction, or {@code Optional.none()} if {@code csv} is invalid.
+     */
+    public static Optional<Transaction> deserialize(String csv) {
+        if ((csv == null) || csv.isEmpty())
+            return Optional.empty();
+
+        String[] tokens = csv.split(",");
+        if (tokens.length != 4)
+            return Optional.empty();
+
+        LocalDateTime dt;
+        try {
+            dt = LocalDateTime.parse(tokens[0]);
+        } catch (DateTimeParseException e) {
+            return Optional.empty();
+        }
+
+        double amt;
+        try {
+            amt = Double.parseDouble(tokens[3]);
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new Transaction(dt, tokens[1], tokens[2], amt));
+    }
+
     @Override
     public String toString() {
         return "[%tY-%<tm-%<td %<tH:%<tM:%<TS] %.2f %s %s for %s".formatted(
@@ -35,5 +68,14 @@ public record Transaction(LocalDateTime dateTime, String description, String ven
      */
     public LocalTime time() {
         return dateTime.toLocalTime();
+    }
+
+    /**
+     * Serializes this transaction into a row of CSV.
+     *
+     * @return A CSV representation of this transaction.
+     */
+    public String serialize() {
+        return "%s,%s,%s,%s".formatted(dateTime, description, vendor, amount);
     }
 }
